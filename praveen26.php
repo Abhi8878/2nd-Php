f3fe5446bad9468d3f74bd69f7c78217c40f0ef1
Abhi8878 <72500053+Abhi8878@users.noreply.github.com>
2022-03-26 21:56:02 +0530

    praveen
    website file

---
+++ praveen26.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action ="newpage.php" method="POST">
+username-:<input type="text" name="t1"><br><br>
+Email  -: &nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="t2"><br><br>
+Address -: &nbsp;<input type="text" name="t3"><br><br>
+         <input type="submit" name="t4">
+</form>
+
+</body>
+</html>
